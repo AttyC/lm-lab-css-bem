913772326018bd2af11e02267e541824672d23e2
Atty Cronin <antoniajcronin@gmail.com>
2023-07-08 19:10:26 +0100

    Amend button click alert to be specific for each character

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-const handleClick = () => alert('Ohh ohh ohh ohh! Can you feel the force?');
+const messages = {
+  darth: 'I Am What You Made Me.',
+  yoda: 'Ooh, ooh, ooh, ooh! Can you feel the Force?',
+  leia: 'Would it help if I got out and pushed?',
+  lando: "How you doin', ya old pirate?",
+};
+const handleClick = (character) => alert(`${messages[character]}`);
